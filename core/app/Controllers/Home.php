9d0780c5ce32ed39796faecaa47b598b1ecd7605
itsupport4 <itsupport4@adisuryasempurna.com>
2025-12-03 08:31:01 +0700

    1

--- core/app/Controllers/Home.php
+++ core/app/Controllers/Home.php
@@ -8,35 +8,7 @@ class Home extends BaseController
 {
     public function __construct() {}
 
-    private function api($url, $params = null, $key = '', $want_decode = true, $want_array = true)
-    {
-
-        $ch = curl_init($url);
-        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-
-        if ($key != '') {
-            curl_setopt($ch, CURLOPT_HTTPHEADER, ["key: $key"]);
-        }
-
-        if (isset($params)) {
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-        }
-
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-
-
-        if ($want_decode) {
-            $data = json_decode(curl_exec($ch), $want_array);
-        } else {
-            $data = curl_exec($ch);
-        }
-
-        curl_close($ch);
-
-        return $data;
-    }
+ 
     private function cek_login_ajax()
     {
         if (session('email_akun') === null) {
